31bd17bf22a82188261a8fc2299211f7f0a6a760
williamzchu <williamzchu@gmail.com>
2022-02-27 02:36:21 -0800

    Shooting mechanic: particles, ammo display, V to switch weapons, r to reload.

--- Clownage/Assets/Scripts/Gun.cs
+++ Clownage/Assets/Scripts/Gun.cs
@@ -1,32 +1,72 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using System.Collections;
 
 namespace StarterAssets
 {
     public class Gun : MonoBehaviour
     {
         public float damage = 2.0f;
-
+        public float firerate = 20.0f;
+        public int magazineSize = 30;
+        private int magazine;
+        public int reserveAmmo = 90;
+        public float reloadSpeed = 2.0f;
+        public Text ammoDisplay; 
         private StarterAssetsInputs _input;
         public Camera gunCam;
+
+        public ParticleSystem bullet;
+
+        private float timeUntilShoot = 0f;
+        private bool reloading;
         void Start()
         {
             _input = GetComponentInParent<StarterAssetsInputs>();
             _input.shoot = false;
+            magazine = magazineSize;
+            reloading = false;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (_input.shoot && !_input.isPaused)
+            if (reloading)
             {
-                Shoot();
-                _input.shoot = false;
+                return;
             }
+            if (_input.shoot && !_input.isPaused && Time.time >= timeUntilShoot && magazine > 0)
+            {
+                timeUntilShoot = Time.time + 1f/firerate;
+                Shoot();
+                magazine--;
+            }
+            ammoDisplay.text = magazine.ToString() + " / " + reserveAmmo.ToString();
+            if (_input.reload && magazine < magazineSize)
+            {
+                StartCoroutine(Reload());
+                return;
+            }
+        }
+
+        IEnumerator Reload()
+        {
+            ammoDisplay.text = "RELOADING";
+            reloading = true;
+            yield return new WaitForSeconds(reloadSpeed);
+
+            int ammoChange = Mathf.Min(magazineSize - magazine, reserveAmmo);
+            magazine = magazine + ammoChange;
+            reserveAmmo = reserveAmmo - ammoChange;
+            ammoDisplay.text = magazine.ToString() + " / " + reserveAmmo.ToString();
+            _input.reload = false;
+            reloading = false;
         }
 
         void Shoot()
         {
+            bullet.Play();
             RaycastHit hit;
             if (Physics.Raycast(gunCam.transform.position, gunCam.transform.forward, out hit))
             {
---
+++ Clownage/Assets/Scripts/Sniper.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace StarterAssets
+{
+    public class Sniper : MonoBehaviour
+    {
+        public float damage = 2.0f;
+        public float firerate = 20.0f;
+        public int magazineSize = 30;
+        private int magazine;
+        public int reserveAmmo = 90;
+
+        private StarterAssetsInputs _input;
+        public Camera gunCam;
+
+        public Text ammoDisplay; 
+        public ParticleSystem bullet;
+        public float reloadSpeed = 4f;
+
+        private float timeUntilShoot = 0f;
+        private bool reloading = false;
+        void Start()
+        {
+            _input = GetComponentInParent<StarterAssetsInputs>();
+            bullet.GetComponent<BulletParticle>().damage = damage;
+            magazine = magazineSize;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {   
+            if (reloading)
+            {
+                return;
+            }
+            if (_input.reload && magazine < magazineSize)
+            {
+                StartCoroutine(Reload());
+                return;
+            }
+            if (_input.shoot && !_input.isPaused && Time.time >= timeUntilShoot && magazine > 0)
+            {
+                timeUntilShoot = Time.time + 1f/firerate;
+                Shoot();
+                magazine--;
+                _input.shoot = false;
+            }
+            ammoDisplay.text = magazine.ToString() + " / " + reserveAmmo.ToString();
+        }
+        IEnumerator Reload()
+        {
+            ammoDisplay.text = "RELOADING";
+            reloading = true;
+            yield return new WaitForSeconds(reloadSpeed);
+
+            int ammoChange = Mathf.Min(magazineSize - magazine, reserveAmmo);
+            magazine = magazine + ammoChange;
+            reserveAmmo = reserveAmmo - ammoChange;
+            ammoDisplay.text = magazine.ToString() + " / " + reserveAmmo.ToString();
+            _input.reload = false;
+            reloading = false;
+        }
+
+        void Shoot()
+        {
+            bullet.Play();
+        }
+
+    }
+}
